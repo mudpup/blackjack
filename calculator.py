b6c459a843bf3dff28afb28aa003053d947c8108
Muddpup <61091752+mudpup@users.noreply.github.com>
2021-03-26 13:11:25 -0500

    Update calculator.py
    Made code easier to understand. Removed unused variables. Fixed bugs.
    Some placeholders removed.

--- calculator.py
+++ calculator.py
@@ -1,28 +1,47 @@
 import math
+from collections import Counter
 
 # This program only works if one deck of cards is used. Please do not use this in a professional setting.
 
 bustChance = 0.0
 doubleDown = " "
 doubleDownFailChance = 0.0
-playerHasAce = False
-dealerHasAce = False
-dealerHandValue = 0
-playerHandValue = 0
 cardValueList = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9,
                  9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 playerHandList = []
 dealerHandList = []
 
-cardvalue = " "
+cardvalue = 0
 dealerstand = False
-dealercardvalue = ""
+dealercardvalue = 0
 
 def calculate():
     print("Placeholder")
+    #playertotal = 0
+    #valuetil21 = 0
+    pace = True in (num == 1 for num in playerHandList)
+    dace = True in (num == 1 for num in dealerHandList)
+    print(str(pace), str(dace))
+    # Player Total Calculation
+    playertotal = sum(playerHandList)
+    print(playertotal)
 
 
-def askvalprint():
+
+def testDealer(): # Prints a certain text depending on the number of the dealer's turn.
+    global dealercardvalue
+    global dealerstand
+    if len(dealerHandList) == 0:
+        dealercardvalue = input("What is the numerical value of the dealer's upcard? ")
+    else:
+        if not dealerstand:
+            dealercardvalue = input("What is the numerical value of the card the dealer just pulled? Write "
+                                        "'n' if the dealer is standing. ")
+            if dealercardvalue == "n":
+                    dealerstand = True
+
+
+def testPlayer(): #Prints a certain text depending on the number of your turn.
     global cardvalue
     global dealerstand
     global dealercardvalue
@@ -31,43 +50,53 @@ def askvalprint():
     elif len(playerHandList) == 1:
         cardvalue = input("What is the numerical value of the second card you were dealt? (Say 'exit' to exit, "
                           "and 'c' to calculate) ")
-        dealercardvalue = input("What is the numerical value of the dealer's upcard? ")
     else:
         cardvalue = input("What is the numerical value of the card you were just dealt? (Say 'exit' to exit, "
                           "and 'c' to calculate) ")
         print(dealerstand)
-        if not dealerstand:
-            dealercardvalue = input("What is the numerical value of the card the dealer just pulled? Write 'N/A' if the dealer is standing. ")
-            if dealercardvalue == "N/A":
-                dealerstand = True
 
-def askvalue():
-    global cardvalue
-    askvalprint()
-    if not str.isdigit(cardvalue) or not int(cardvalue) <= 10:
-        if cardvalue == 'exit' or cardvalue == 'c':
-            if cardvalue == 'exit':
-                exit()
-            else:
-                calculate()
-        elif not str.isdigit(cardvalue):
-            print("Invalid response. Please input the numerical value of your card.")
-            askvalue()
+def askPlayervalue():
+    testPlayer()
+    if not str.isdigit(cardvalue):
+        if cardvalue == 'exit':
+            exit()
+        elif cardvalue == 'c':
+            calculate()
         else:
-            print("Your card value cannot be higher than 10.")
-            askvalue()
-    elif not int(dealercardvalue)<= 10 or not dealercardvalue == "N/A":
-        print("Invalid response. Please input the numerical value of the dealer's card.")
-        askvalue()
+            print("Invalid response. Please input the numerical value of your card.")
+            askPlayervalue()
+    elif int(cardvalue) > 10:
+        print("Your card value cannot be higher than 10.")
+        askPlayervalue()
     else:
         playerHandList.append(int(cardvalue))
         cardValueList.remove(int(cardvalue))
         print(playerHandList)
-        if not dealerstand:
-            dealerHandList.append(int(dealercardvalue))
-            cardValueList.remove(int(dealercardvalue))
+        askDealervalue()
+
+def askDealervalue(): # Checks the value of the dealer's card.
+    global CallDealer
+    global dealerstand
+    testDealer()
+    if not str.isdigit(dealercardvalue):
+        if dealercardvalue == 'exit':
+            exit()
+        elif dealercardvalue == 'c':
+            calculate()
+        elif dealercardvalue == 'n' and len(dealerHandList) > 0:
+            dealerstand = True
+            askPlayervalue()
+        else:
+            print("Invalid response. Please input the numerical value of your card.")
+            askDealervalue()
+    elif int(dealercardvalue) > 10:
+        print("Your card value cannot be higher than 10.")
+        askDealervalue()
+    else:
+        dealerHandList.append(int(dealercardvalue))
+        cardValueList.remove(int(dealercardvalue))
         print(dealerHandList)
-        askvalue()
+        askPlayervalue()
 
-
-askvalue()
+#askPlayervalue()
+calculate()
